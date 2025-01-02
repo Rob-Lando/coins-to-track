d903d64ccb1d62f41afb6f2c38363b44db28a820
Rob-Lando <Roblando95@outlook.com>
2025-01-02 02:43:31 -0500

    first iteration of extract script

---
+++ src/extract.py
@@ -0,0 +1,138 @@
+import requests
+import polars as pl
+import json
+import os
+from datetime import datetime,timezone
+
+
+
+
+def parse_args():
+    pass
+
+def cmc_extract(cmc_endpoint_url: str, headers: dict, parameters: dict) -> dict:
+
+    """
+    returns: dict api response 
+    """
+
+    # coins_to_track = pl.read_csv("coins_to_track.csv")
+    # symbols = ','.join(ctt['Symbol'])
+
+    # parameters.update(symbol=symbols)
+    session = requests.Session()
+    session.headers.update(headers)
+
+    responses = json.loads(session.get(cmc_endpoint_url, params = parameters).text)
+
+    return responses
+
+
+
+def get_quotes(cmc_endpoint_url: str, headers: dict, parameters: dict, csv_write_path: str) -> None:
+
+    responses = cmc_extract(cmc_endpoint_url = cmc_endpoint_url, headers = headers, parameters = parameters)
+    
+    ts = datetime.now(timezone.utc)
+
+    returned_data = responses["data"]
+    returned_symbols = list(returned_data.keys())
+
+    # list comp to build list of LazyFrames (Lazy evaluated DataFrame) with quote data for each symbol
+    # and adding symbol identifier, IsTopCurrency, and LoadedWhen columns to each chunk
+    quote_lfs = [
+        pl.LazyFrame(data["quote"]["USD"]).with_columns(
+                pl.lit(symbol).alias("symbol"),
+                pl.lit(int(returned_data[symbol]["cmc_rank"])<=10).alias("IsTopCurrency"),
+                pl.lit(ts.strftime("%Y-%m-%dT%H:%M:%SZ")).alias("LoadedWhen")
+            ) 
+        for symbol,data in returned_data.items()
+    ]
+
+    # concat quote dfs to one single LazyFrame
+    lf = pl.concat(quote_dfs, how = 'vertical_relaxed')
+
+    # materialize to DataFrame w/ collect() and write data to local path as csv
+    lf.collect().write_csv(file = fr"{csv_write_path}/{ts.strftime('%Y%m%dT%H%M%S')}_quotes.csv")
+
+    return None
+
+
+
+def get_metadata(cmc_endpoint_url: str, headers: dict, parameters: dict, csv_write_path: str) -> None:
+
+    responses = cmc_extract(cmc_endpoint_url = cmc_endpoint_url, headers = headers, parameters = parameters)
+    
+    ts = datetime.now(timezone.utc)
+
+    returned_data = responses["data"]
+    returned_symbols = list(returned_data.keys())
+
+    meta_dfs = [pl.from_records(data) for symbol,data in returned_data.items()]
+    meta_df = pl.concat([df for df in meta_dfs if df.shape != (0,0)], how = "vertical_relaxed")
+
+
+    # Unnesting contract_address, platform, and urls nested fields
+    meta_df = \
+        meta_df.with_columns(
+
+            pl.col("contract_address").list.first().struct.rename_fields(["contract_address","contract_address.platform"])
+
+        ).unnest("contract_address").with_columns(
+
+            pl.col("contract_address.platform").struct.rename_fields(["contract_address.platform.name","contract_address.platform.coin"])
+
+        ).unnest("contract_address.platform").with_columns(
+
+            pl.col("contract_address.platform.coin").struct.rename_fields(["contract_address.platform.coin.id","contract_address.platform.coin.name","contract_address.platform.coin.symbol","contract_address.platform.coin.slug"])
+
+        ).unnest("contract_address.platform.coin").with_columns(
+
+            pl.col("platform").struct.rename_fields(["platform.id","platform.name","platform.slug","platform.symbol","platform.token_address"])
+
+        ).unnest(["platform","urls"])
+
+    
+    meta_df = meta_df.with_columns(pl.lit(ts.strftime("%Y-%m-%dT%H:%M:%SZ")).alias("LoadedWhen"))
+
+
+    for column,type in dict(zip(meta_df.columns,meta_df.dtypes)).items():
+        # Convert fields of type List[] to pipe delimited strings so we can write to csv
+        if type == pl.List:
+            meta_df = meta_df.with_columns(pl.col(column).list.join("|"))
+
+
+    meta_df.write_csv(file = fr"src/extracts/metadata/{ts.strftime('%Y%m%dT%H%M%S')}_metadata.csv")
+
+    return meta_df
+
+def main():
+
+    CLARGS = parse_args()
+
+    coins_to_track = pl.read_csv("./coins_to_track.csv")
+    symbols = ','.join(coins_to_track['Symbol'])
+    cmc_key = os.getenv("CMC_KEY")
+
+    headers = {
+        "X-CMC_PRO_API_KEY": cmc_key,
+        "Content-Type": "application/json"
+    }
+
+    get_quotes(        
+        cmc_endpoint_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest", 
+        headers = headers, 
+        parameters = {"symbol":symbols, "skip_invalid": "true"}, 
+        csv_write_path = "src/extracts/metadata"
+    )
+
+    get_metadata(
+        cmc_endpoint_url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info", 
+        headers = headers, 
+        parameters = {"symbol":symbols, "skip_invalid": "true"}, 
+        csv_write_path = "src/extracts/metadata"
+    )
+    pass
+
+if __name__ == "__main__":
+    main()
